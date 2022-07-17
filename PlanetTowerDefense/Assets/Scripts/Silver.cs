c7c0df7578e3c2bf7bea2a570423f6ccd9bf9785
ChariotRider <109397813+ChariotRider@users.noreply.github.com>
2022-07-16 23:01:46 -0500

    Updated Silver

--- PlanetTowerDefense/Assets/Scripts/Silver.cs
+++ PlanetTowerDefense/Assets/Scripts/Silver.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Silver : Interactable {
     public override IEnumerator Interact(){
         Debug.Log("Mining Silver");
+        FindObjectOfType<GameManager>().GetComponentInParent<InventoryManager>().updateSilver(25);
+
+        
         yield return new WaitForSeconds(1f);
     }
 }
