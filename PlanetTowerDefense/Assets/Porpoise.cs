b76487884a0cceea340e1d0e164c35affa967949
Fisher Heins <fisher@heinsdesign.com>
2022-07-17 07:08:01 -0500

    Update Porpoise.cs

--- PlanetTowerDefense/Assets/Porpoise.cs
+++ PlanetTowerDefense/Assets/Porpoise.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Porpoise : Interactable
 {
+    private int health = 500;
+    public static Porpoise Instance;
+    [SerializeField]
+    private ParticleSystem engine;
+    [SerializeField]
+    private AudioSource engineRumble;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     public override IEnumerator Interact()
     {
         FindObjectOfType<CameraManager>().SwapToStrat();
@@ -11,4 +22,20 @@ public class Porpoise : Interactable
         yield return null;
     }
 
+    public void TakeDamage()
+    {
+        health--;
+
+        if(health <= 0)
+        {
+            //YOU LOse
+        }
+    }
+
+    public void WinGame()
+    {
+        gameObject.transform.Translate(Vector3.up * 100);
+        engine.Play();
+        engineRumble.Play();
+    }
 }
